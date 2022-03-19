94433cf5b8b1af6e8737dfa932e038a187642f55
brendanjamesmulhern <brendanjamesmulhern@gmail.com>
2022-03-19 12:47:03 -0400

    deploy

--- smart_goals/settings.py
+++ smart_goals/settings.py
@@ -132,5 +132,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'client/build/static'),
 # ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
